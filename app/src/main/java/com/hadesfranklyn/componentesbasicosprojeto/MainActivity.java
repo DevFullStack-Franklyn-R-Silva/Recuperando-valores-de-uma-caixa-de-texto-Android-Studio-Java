f3a8985fe4f2f88cf09f3a8ece1c8a7aebc9fc96
Franklyn R <franklyn1998@live.com>
2022-07-27 12:00:27 -0300

    Caixa de seleção

--- app/src/main/java/com/hadesfranklyn/componentesbasicosprojeto/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/componentesbasicosprojeto/MainActivity.java
@@ -2,6 +2,7 @@ package com.hadesfranklyn.componentesbasicosprojeto;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText campoNome;
     private TextInputEditText campoEmail;
     private TextView textoResultado;
+    private CheckBox checkVerde, checkBranco, checkVermelho;
 
 
     @Override
@@ -24,18 +26,44 @@ public class MainActivity extends AppCompatActivity {
         campoNome = findViewById(R.id.editNome);
         campoEmail = findViewById(R.id.editEmail);
         textoResultado = findViewById(R.id.textResultado);
+
+        //CheckBox
+        checkVerde = findViewById(R.id.checkVerde);
+        checkBranco = findViewById(R.id.checkBranco);
+        checkVermelho = findViewById(R.id.checkVermelho);
+    }
+
+    public String checkbox() {
+
+        String texto = "", corSelecionada = "";
+
+        if (checkVerde.isChecked()) {
+            corSelecionada = checkVerde.getText().toString();
+            texto = corSelecionada+" selecionado - ";
+        }
+        if (checkBranco.isChecked()) {
+            corSelecionada = checkBranco.getText().toString();
+            texto = texto + corSelecionada+" selecionado - ";
+        }
+        if (checkVermelho.isChecked()) {
+            corSelecionada = checkVermelho.getText().toString();
+            texto = texto + corSelecionada+" selecionado - ";
+        }
+        return texto;
+
     }
 
     public void enviar(View view) {
+        checkbox();
 
         String nome = campoNome.getText().toString();
         String email = campoEmail.getText().toString();
 
-        textoResultado.setText("nome: "+nome +"\nemail: " + email);
+        textoResultado.setText("nome: " + nome + "\nemail: " + email + "\n" + checkbox());
     }
 
     public void limpar(View view) {
-        campoNome.setText("");;
+        campoNome.setText("");
         campoEmail.setText("");
         textoResultado.setText("");
     }
